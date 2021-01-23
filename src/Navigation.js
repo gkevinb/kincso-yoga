a5ca709bf6d75372d797b1b293812e60aba8924d
Gabor Kevin Barta <gkevinba@gmail.com>
2021-01-23 19:15:14 +0100

    Add mobile navigation menu overlay feature

---
+++ src/Navigation.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./Navigation.css";
+
+function Navigation() {
+    const [toggle, setToggle] = useState(false);
+    const [width, setWidth] = useState("0%");
+
+    console.log(toggle);
+
+    const openMenu = () => {
+        setToggle(!toggle);
+    };
+    useEffect(() => {
+        if (toggle) {
+            document.body.style.overflow = "hidden";
+            setWidth("100%")
+        } else {
+            document.body.style.overflow = "unset";
+            setWidth("0%")
+        }
+    }, [toggle]);
+
+    return (
+        <div className="navigation">
+            <div className="navigation__modal" style={{width: width}}>
+                <h1>Home</h1>
+                <h1>Blog</h1>
+                <h1>About</h1>
+            </div>
+
+            <div onClick={openMenu} className="navigation__menu">
+                <MenuIcon fontSize="large" />
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
